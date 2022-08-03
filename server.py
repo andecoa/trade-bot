ecab87199fc6ec4269ade3cf2772437a991f8cc6
Angelo Aldecoa <angelokn14@gmail.com>
2022-08-03 10:41:52 -0700

    install and set up black code formatter

--- server.py
+++ server.py
@@ -3,10 +3,11 @@ from sanic.response import text, json
 
 app = Sanic("TradeBot")
 
+
 @app.get("/trade")
 async def trade(request):
     return text("Trade Bot")
 
+
 if __name__ == "__main__":
     app.run(dev=True)
- 

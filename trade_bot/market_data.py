8051a0f6ae2087f118ffb569228b3fe1fdcfd970
Benny Song <bzs@hotmail.ca>
2022-08-21 11:25:27 -0700

    switched to api implementation and extracted key to environment variable

--- trade_bot/market_data.py
+++ trade_bot/market_data.py
@@ -1,15 +1,23 @@
-import requests
+import finnhub
 import time
+import os
 
 
-def getMarketData(symbol: str):
-    """Returns the price for a given symbol"""
-    x = requests.get(
-        "https://finnhub.io/api/v1/quote",
-        params={"symbol": symbol, "token": "cbsjggaad3i9sd7nbvp0"},
-    )
+class MarketData:
+    key = os.environ["API_KEY"]
 
-    if x.status_code == 200:
-        return float(x.text.split(",")[0][5:])
-    else:
-        return None
+    def __init__(self):
+        self.finnhub_client = finnhub.Client(api_key=MarketData.key)
+
+    def quote(self, symbol: str):
+        """Returns the price for a given symbol"""
+        return self.finnhub_client.quote(symbol)["c"]
+
+
+"""
+md = MarketData()
+
+while True:
+    print(md.quote("BINANCE:BTCUSDT"))
+    time.sleep(60)
+"""
